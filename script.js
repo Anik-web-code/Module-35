0d1ef02db3fd88b1055f177b356afa1d4226a016
Anik-web-code <alianik11star@gmail.com>
2025-03-15 11:41:13 +0600

    added videos

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const loadButtons = (categories) => {
   for (cat of categories) {
     const btnContainer = document.createElement("div");
     btnContainer.innerHTML = `
-        <button class="btn hover:bg-red-500 hover:text-[#FFFFFF]">${cat.category}</button>
+        <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn hover:bg-red-500 hover:text-[#FFFFFF]">${cat.category}</button>
         `;
     container.appendChild(btnContainer);
   }
@@ -18,14 +18,31 @@ const loadButtons = (categories) => {
 
 loadCategories();
 
+const loadCategoryVideos = (id) => {
+  if (id === 1005) {
+    loadError();
+  } else {
+    url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+        .then((data) => {
+            const clicked = document.getElementById(`btn-${id}`);
+            clicked.classList.add("active");
+            showVideos(data.category)
+        });
+  }
+};
+
 const loadVideos = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
-    .then((data) => showVideos(data.videos));
+      .then((data) => showVideos(data.videos)
+      );
 };
 
 const showVideos = (cards) => {
   const videoContainer = document.getElementById("main");
+  videoContainer.innerHTML = "";
   for (card of cards) {
     const videoCard = document.createElement("div");
     videoCard.innerHTML = `
@@ -48,8 +65,8 @@ const showVideos = (cards) => {
   <h1 class="ml-20">${card.authors[0].profile_name}</h1>
   ${
     card.authors[0].verified
-      ? '<img class="w-4 h-4" src="verified-icon.png" alt="Verified">'
-      : ""
+      ? ""
+      : '<img class="w-4 h-4" src="verified-icon.png" alt="Verified">'
   }
 </div>
 <div class="ml-20 pb-3">
@@ -62,4 +79,22 @@ ${card.others.views} views
   }
 };
 
-loadVideos();
+const loadError = () => {
+  const videoContainer = document.getElementById("main");
+  videoContainer.innerHTML = "";
+  const sorry = document.createElement("div");
+  sorry.innerHTML = `
+      <div class="flex justify-center items-center">
+        <div class="text-center">
+          <img src="icon.png" class="mx-auto" alt="Error Icon">
+          <h1 class="text-3xl mt-4 font-bold">Oops!! Sorry, There is no<br> content here</h1>
+        </div>
+      </div>
+    `;
+  sorry.style.position = "absolute"; // Position it absolutely on the screen
+  sorry.style.top = "50%"; // Vertically center
+  sorry.style.left = "50%"; // Horizontally center
+  sorry.style.transform = "translate(-50%, -50%)"; // Adjust by 50% to center exactly
+
+  videoContainer.appendChild(sorry);
+};
